4048caa502f5f0e1172a89ea3f1017e4b9ea3f1b
NorCz <norbert.czarnomski@yahoo.com>
2022-12-03 10:27:33 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter,
+  Routes
+} from "react-router-dom";
+import Form from "./Form";
+import Home from "./Home";
  
 class App extends Component {
   render() {
     return (
-      <></>
+      <HashRouter>
+        <Home/>
+    <div className="components">
+        <Routes>
+            <Route path="/" element={<Home/>} exact="true" />
+            <Route path="/contact" element={<Form/>} exact="true" />
+        </Routes>
+    </div>
+      </HashRouter>
     );
   }
 }
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom"; 
-
-class Form extends Component {
+ import { NavLink } from "react-router-dom";
+class Home extends Component {
   render() {
     return (
-        <body>
-        <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-light">
-        <NavLink class="navbar-brand text-info" id="home" exact="true" to="/">Home</NavLink>
+    <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-light">
+        <NavLink class="navbar-brand text-info" id="home" exact to="/">Home</NavLink>
         <div className="collapse navbar-collapse" id="rest">
             <ul className="navbar-nav">
             <li className="nav-item" id="rest-li">
@@ -18,15 +16,8 @@ class Form extends Component {
             </ul>
         </div>
     </nav>
-        <div>
-            <h1>Something</h1>
-            <form>
-
-            </form>
-        </div>
-        </body>
     );
   }
 }
  
-export default Form;
+export default Home;
